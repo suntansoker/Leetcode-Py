23108ae385aa34b4c6dbcd1be9ce054543187789
Sayantan Sarkar <sayantanthepointer@gmail.com>
2023-06-08 17:29:39 -0700

    smallest-int-target

---
+++ daily-problems/744-smallest-int-greater-than-target.py
@@ -0,0 +1,35 @@
+'''
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ 
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+'''
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        ch = letters[0]
+        for i in range(len(letters)):
+            c = letters[i]
+            if c > target:
+                ch = c
+                break
+
+        return ch
