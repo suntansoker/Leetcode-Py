68dbce0dc1e466f73064dd041e498c75e86f1372
Sayantan Sarkar <sayantanthepointer@gmail.com>
2023-07-30 23:11:39 -0700

    min-ascii-delete

--- contests/weekly-contests/contest-348/2719-count-of-integers.py
+++ contests/weekly-contests/contest-348/2719-count-of-integers.py
@@ -26,3 +26,50 @@ Constraints:
 1 <= num1 <= num2 <= 1022
 1 <= min_sum <= max_sum <= 400
 '''
+
+
+class Solution:
+    def count(self, num1: str, num2: str, min_sum: int, max_sum: int) -> int:
+        dp = {}
+        n1 = len(num1)
+        n2 = len(num2)
+
+        MOD = 10**9 + 7
+
+        num1 = '0' * (n2-n1) + num1
+        print(num1)
+
+        def findNumbers(idx, tightLow, tightHigh, sm):
+            if sm < 0:
+                return 0
+
+            if idx == n2:
+                return 1
+
+            low, high = num1[idx], num2[idx]
+
+            if (idx, tightLow, tightHigh, sm) in dp:
+                return dp[(idx, tightLow, tightHigh, sm)]
+
+            if tightLow:
+                low = int(num1[idx])
+            else:
+                low = 0
+
+            if tightHigh:
+                high = int(num2[idx])
+            else:
+                high = 9
+
+            count = 0
+            for i in range(low, high+1):
+                count = (count + findNumbers(idx+1, tightLow and int(
+                    num1[idx]) == i, tightHigh and int(num2[idx]) == i, sm-i)) % MOD
+
+            dp[(idx, tightLow, tightHigh, sm)] = count
+            return dp[(idx, tightLow, tightHigh, sm)]
+
+        minNos = findNumbers(0, 1, 1, min_sum-1)
+        maxNos = findNumbers(0, 1, 1, max_sum)
+
+        return (maxNos-minNos) % MOD
