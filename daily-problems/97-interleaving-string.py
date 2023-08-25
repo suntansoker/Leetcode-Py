98806b9c8d8fc4b3a05bc1d7e9d25a81e5dd7a8f
Sayantan Sarkar <sayantanthepointer@gmail.com>
2023-08-24 22:50:43 -0700

    interleave-str

---
+++ daily-problems/97-interleaving-string.py
@@ -0,0 +1,68 @@
+'''
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+
+An interleaving of two strings s and t is a configuration where s and t are divided into n and m 
+substrings
+ respectively, such that:
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+
+ 
+
+Example 1:
+
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Explanation: One way to obtain s3 is:
+Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
+Since s3 can be obtained by interleaving s1 and s2, we return true.
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
+Example 3:
+
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+ 
+
+Constraints:
+
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lowercase English letters.
+'''
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        n3 = len(s3)
+
+        @cache
+        def findInterLeave(idx1, idx2, idx3):
+            if idx1 == n1 and idx2 == n2 and idx3 == n3:
+                return True
+
+            if idx3 == n3:
+                return False
+
+            res = False
+
+            if idx1 < n1 and idx3 < n3 and s1[idx1] == s3[idx3]:
+                res = res or findInterLeave(idx1+1, idx2, idx3+1)
+
+            if idx2 < n2 and idx3 < n3 and s2[idx2] == s3[idx3]:
+                res = res or findInterLeave(idx1, idx2+1, idx3+1)
+
+            return res
+
+        return findInterLeave(0, 0, 0)
